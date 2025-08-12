b740bcc2c52230288d83b1fd4ceef15f0fb2de66
hanar3 <vh.morenodasilva@gmail.com>
2025-08-12 20:40:24 -0300

    assertion tests

---
+++ order-book/tests/order_book.rs
@@ -0,0 +1,32 @@
+use order_book::book::OrderBook;
+use order_book::defs::items::Side;
+
+fn setup_book() -> OrderBook {
+    let mut book = OrderBook::new();
+    for i in 0..1000 {
+        book.add_limit_order(Side::Buy, 9999 - i, 10);
+        book.add_limit_order(Side::Sell, 10001 + i, 10);
+    }
+    book
+}
+
+#[test]
+fn add_limit_order_no_match() {
+    let mut book = setup_book();
+    let trades = book.add_limit_order(Side::Buy, 9000, 10);
+    assert_eq!(trades.len(), 0);
+}
+
+#[test]
+fn add_limit_order_full_match_one() {
+    let mut book = setup_book();
+    let trades = book.add_limit_order(Side::Buy, 10001, 10);
+    assert_eq!(trades.len(), 1);
+}
+
+#[test]
+fn add_limit_order_walk_the_book() {
+    let mut book = setup_book();
+    let trades = book.add_limit_order(Side::Buy, 10005, 50);
+    assert_eq!(trades.len(), 5);
+}

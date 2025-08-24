02ab9142c869ce1288be93547d12503523195da8
hanar3 <vh.morenodasilva@gmail.com>
2025-08-24 19:22:47 -0300

    trades migration

---
+++ migrations/20250824221319_create-trades-table.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+CREATE TABLE trades (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  taker_order_id INTEGER NOT NULL,
+  maker_order_id INTEGER NOT NULL,
+  filled_qty INTEGER,
+  FOREIGN KEY (taker_order_id) REFERENCES orders(orders),
+  FOREIGN KEY (maker_order_id) REFERENCES orders(name)
+);
